f9b047b401434c541709972cbd9e4c855b8a2c1b
Xinbo Wang <xinbo.wang.dt@gmail.com>
2020-02-02 18:36:29 -0800

    Read data from markdown

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,24 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `Title from siteMetadata`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
 }
---
+++ src/pages/about.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/layout'
+export default ({data}) => (
+  <Layout>
+    <h1>About {data.site.siteMetadata.title}</h1>
+    <p>
+      Lwersdfdsgdadg sdfadf asdgadf gdfg fdg fdgfdsgd sfgsfdg
+    </p>
+  </Layout>
+)
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
